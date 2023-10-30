d119e39e4ea62f2e6376e5decc30641bfcda3314
Christyfernandes <christyabcd.05@gmail.com>
2023-10-30 18:23:37 +0530

    added pdf-genrator API

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM node:14
+
+WORKDIR /usr/src/app
+
+# Dependencies for headless chrome - puppeteer
+RUN echo "deb http://archive.debian.org/debian/ jessie main" > /etc/apt/sources.list
+RUN echo "deb-src http://archive.debian.org/debian/ jessie main" >> /etc/apt/sources.list
+RUN echo "deb http://archive.debian.org/debian-security/ jessie/updates main" >> /etc/apt/sources.list
+RUN echo "deb-src http://archive.debian.org/debian-security/ jessie/updates main" >> /etc/apt/sources.list
+RUN apt-get -o Acquire::Check-Valid-Until=false update
+RUN apt install -y libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 --allow-unauthenticated
+RUN apt-get update
+RUN apt-get install -y wget gnupg && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+RUN apt-get update && apt-get install -y fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst fonts-freefont-ttf libxss1 --no-install-recommends --allow-unauthenticated
+RUN apt-get install -y fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst fonts-freefont-ttf libxss1 --no-install-recommends --allow-unauthenticated --allow-unauthenticated
+RUN apt-get install -y libdrm2 libgbm1 libnss3 --allow-unauthenticated
+
+
+RUN mkdir -p /usr/src/app/user_upload
+RUN mkdir -p /usr/src/app/logs
+
+COPY package*.json ./
+RUN npm install --only=production
+COPY dist/ .
+
+EXPOSE 8080
+
+CMD [ "node", "index.js" ]
+
---
+++ app.ts
@@ -0,0 +1,51 @@
+import express from 'express';
+import { v4 as uuidv4 } from 'uuid'
+import { logError } from './utils/logger'
+const app = express();
+const port = 3000;
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
+const puppeteer = require('puppeteer');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
+
+
+app.post('/public/v8/course/batch/cert/download/mobile', async (req, res) => {
+  try {
+    const svgContent = req.body.printUri
+    if (req.body.outputFormat === 'svg') {
+      const _decodedSvg = decodeURIComponent(svgContent.replace(/data:image\/svg\+xml,/, '')).replace(/\<!--\s*[a-zA-Z0-9\-]*\s*--\>/g, '')
+      res.type('html')
+      res.status(200).send(_decodedSvg)
+    } else if (req.body.outputFormat === 'pdf') {
+      const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] })
+      const page = await browser.newPage()
+      await page.goto(svgContent, { waitUntil: 'networkidle2' })
+      const uuid = uuidv4()
+      const buffer = await page.pdf({ path: `certificates/certificate-${uuid}.pdf`, printBackground: true, width: '1204px', height: '662px' })
+      res.set({ 'Content-Type': 'application/pdf', 'Content-Length': buffer.length })
+      res.send(buffer)
+      browser.close()
+    } else {
+      res.status(400).json({
+        error: 'Unsupported output format',
+        msg: 'Output format should be svg or pdf',
+      })
+    }
+  } catch (err) {
+    logError(err)
+
+    res.status((err && err.response && err.response.status) || 500).send(
+      (err && err.response && err.response.data) || {
+        error: 'Failed due to unknown reason',
+      }
+    )
+  }
+})
